de1c7ddf6a7c9bc5392a7914a5f78a8a4223eb09
Pascal Perle <Pascal@FamiliePerle.de>
2019-09-14 11:23:53 +0200

    ✨ parallel a* search (no real world speed improvements 😞 )

--- DeepCubeA/a_star_search.py
+++ DeepCubeA/a_star_search.py
@@ -95,7 +95,7 @@ def heuristic(env, model):
     return predict.detach().cpu().numpy()[0][0]
 
 
-def search_way(start_env, model, epsilon=0.000000001, weight=1):
+def search_way(start_env, model, epsilon=0.000000001, weight=1, progress_bar_update_iterations=10):
     x = sum((start_env.room_state > 0).flatten())
     progress_bar = tqdm(total=int(x * binom(x - 1, start_env.num_boxes)))
 
@@ -143,8 +143,8 @@ def search_way(start_env, model, epsilon=0.000000001, weight=1):
         remove_from_open_heap_md5_dict(fscore[current_md5], current_md5)
 
         counter += 1
-        if counter % 1000 == 0:
-            progress_bar.update(1000)
+        if counter % progress_bar_update_iterations == 0:
+            progress_bar.update(progress_bar_update_iterations)
             if counter % 10000 == 0:
                 gc.collect()
 
@@ -154,6 +154,7 @@ def search_way(start_env, model, epsilon=0.000000001, weight=1):
                 steps.extend(actions[current_md5])
                 current_md5 = came_from[current_md5]
 
+            progress_bar.update(counter % progress_bar_update_iterations)
             progress_bar.close()
             return list(reversed(steps)), len(close_set)
 
@@ -179,6 +180,124 @@ def search_way(start_env, model, epsilon=0.000000001, weight=1):
                 if neighbor_env_md5 not in actions:
                     actions[neighbor_env_md5] = []
                 actions[neighbor_env_md5].append(action)
+                while fscore[neighbor_env_md5] in open_heap_fscore_set:
+                    fscore[neighbor_env_md5] += epsilon
+                heappush(open_heap, (fscore[neighbor_env_md5], [neighbor_env.room_state, neighbor_env.room_fixed, neighbor_env.done]))
+                add_to_open_heap_md5_dict(fscore[neighbor_env_md5], neighbor_env_md5)
+
+    progress_bar.close()
+    return False, False
+
+
+def heuristic_parallel(envs, model):
+    states = torch.from_numpy(np.asarray([])).flatten()
+    for env in envs:
+        wall_map = torch.from_numpy((env.room_state == 0).astype(int)).flatten()
+        target_map = torch.from_numpy((env.room_fixed == 2).astype(int)).flatten()
+        boxes_map = torch.from_numpy(((env.room_state == 3) | (env.room_state == 4)).astype(int)).flatten()
+        agent_map = torch.from_numpy((env.room_state == 5).astype(int)).flatten()
+
+        state = torch.cat((wall_map, target_map, boxes_map, agent_map), 0)
+        states = torch.cat((states.float(), state.float()), 0)
+    states = states.reshape(len(envs), -1)
+
+    with torch.no_grad():
+        predict = model(states.float().to(DEVICE))
+
+    return predict.detach().cpu().numpy().flatten()
+
+
+def search_way_parallel(start_env, model, epsilon=0.000000001, weight=1, progress_bar_update_iterations=10):
+    x = sum((start_env.room_state > 0).flatten())
+    progress_bar = tqdm(total=int(x * binom(x - 1, start_env.num_boxes)))
+
+    start_env_md5 = hashlib.md5(marshal.dumps(start_env.room_state)).hexdigest()
+
+    close_set = set()
+    came_from = {}
+    gscore = {start_env_md5: 0}
+    fscore = {start_env_md5: heuristic_parallel([start_env], model)[0]}
+    open_heap = []
+    open_heap_md5_dict = {}
+    open_heap_fscore_set = set()
+    actions = {}
+
+    def add_to_open_heap_md5_dict(fscore, key):
+        if key in open_heap_md5_dict:
+            open_heap_md5_dict[key] += 1
+        else:
+            open_heap_md5_dict[key] = 1
+
+        open_heap_fscore_set.add(fscore)
+
+    def remove_from_open_heap_md5_dict(fscore, key):
+        open_heap_md5_dict[key] -= 1
+        if open_heap_md5_dict[key] == 0:
+            del open_heap_md5_dict[key]
+
+        if fscore in open_heap_fscore_set:
+            open_heap_fscore_set.remove(fscore)
+
+    heappush(open_heap, (fscore[start_env_md5], [start_env.room_state, start_env.room_fixed, False]))
+    add_to_open_heap_md5_dict(fscore[start_env_md5], start_env_md5)
+
+    counter = 0
+    current = deepcopy(start_env)
+    current.set_maxsteps(999999)
+    while open_heap:
+        current.room_state, current.room_fixed, current.done = heappop(open_heap)[1]  # pop the smallest item off the heap
+        player_position = np.where(current.room_state == 5)
+        if len(player_position[0]) > 1:
+            print('ERROR', player_position)
+        current.player_position = np.array([player_position[0][0], player_position[1][0]])
+
+        current_md5 = hashlib.md5(marshal.dumps(current.room_state)).hexdigest()
+        remove_from_open_heap_md5_dict(fscore[current_md5], current_md5)
+
+        counter += 1
+        if counter % progress_bar_update_iterations == 0:
+            progress_bar.update(progress_bar_update_iterations)
+            if counter % 10000 == 0:
+                gc.collect()
+
+        if current.done:
+            steps = []
+            while current_md5 in came_from:
+                steps.extend(actions[current_md5])
+                current_md5 = came_from[current_md5]
+
+            progress_bar.update(counter % progress_bar_update_iterations)
+            progress_bar.close()
+            return list(reversed(steps)), len(close_set)
+
+        close_set.add(current_md5)
+        temp_parallel = []
+        for action in [1, 2, 3, 4]:  # skip 0 as NOP
+            tentative_g_score = gscore[current_md5] + 1
+
+            neighbor_env = deepcopy(current)
+            _, _, neighbor_env.done, _ = neighbor_env.step(action)
+
+            neighbor_env_md5 = hashlib.md5(marshal.dumps(neighbor_env.room_state)).hexdigest()
+
+            if np.array_equal(current.room_state, neighbor_env.room_state):  # nothing changed
+                continue
+
+            if neighbor_env_md5 in close_set and tentative_g_score >= gscore.get(neighbor_env_md5, 0):
+                continue
+
+            if tentative_g_score < gscore.get(neighbor_env_md5, 0) or neighbor_env_md5 not in open_heap_md5_dict:
+                came_from[neighbor_env_md5] = current_md5
+                gscore[neighbor_env_md5] = tentative_g_score
+                if neighbor_env_md5 not in actions:
+                    actions[neighbor_env_md5] = []
+                actions[neighbor_env_md5].append(action)
+
+                temp_parallel.append([neighbor_env_md5, neighbor_env])
+
+        if len(temp_parallel) > 0:
+            for heuristic_result, (neighbor_env_md5, neighbor_env) in zip(heuristic_parallel([neighbor_env for _, neighbor_env in temp_parallel], model), temp_parallel):
+                fscore[neighbor_env_md5] = gscore[neighbor_env_md5] * weight + heuristic_result
                 while fscore[neighbor_env_md5] in open_heap_fscore_set:
                     fscore[neighbor_env_md5] += epsilon
                 heappush(open_heap, (fscore[neighbor_env_md5], [neighbor_env.room_state, neighbor_env.room_fixed, neighbor_env.done]))
